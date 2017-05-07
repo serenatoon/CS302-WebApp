83bff28e78006f99e9a57c59e094aef5d854d1bd
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-05-07 14:41:00 +1200

    return correct IP across platforms

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import socket
 import webbrowser
 import os
 
+
 def getIP():
 	try:
-		return socket.gethostbyname(socket.gethostname())
+		ip = socket.gethostbyname(socket.getfqdn()) # return fully-qualified domain name
 	except:
-		return "localhost"	
+		ip = ''
+	if (not ip) or (ip.startswith('127.')): 
+	# linux returns localhost 127.0.0.1, but we need eth0 IP 
+	# sourced from http://stackoverflow.com/questions/24196932/how-can-i-get-the-ip-address-of-eth0-in-python
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create new socket 
+		s.connect(("8.8.8.8", 80)) # open socket to google's DNS server 
+		return s.getsockname()[0] # take address from that connection 
+
+	return ip
 
 ip = getIP() # socket to listen  
 port = 10032 # TCP port to listen on 
@@ -21,11 +30,9 @@ class main(object):
 
 	webbrowser.open_new('http://%s:%d/home' % (ip, port))
 
-
-
 def runMain():
 	conf = {
-	'/': {
+		'/': {
 		'tools.sessions.on': True, # enable sessions to synchronise activity between users 
 		'tools.staticdir.root:': os.path.abspath(os.getcwd()) # serve whole dir
 		}
@@ -42,5 +49,7 @@ def runMain():
 	cherrypy.engine.start() # start webserver
 
 	cherrypy.engine.block() # stop doing anything else 
+	#cherrypy.engine.stop() # terminate; stop the channel of the bus 
+	cherrypy.server.unsubscribe() # disable built-in HTTP server 
 
 runMain()
