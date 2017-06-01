9d066014acf843b527094ad067c52b835fbc6a90
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-06-02 02:42:47 +1200

    i did a lot of shit i forgot

--- main.py
+++ main.py
@@ -10,199 +10,203 @@ import sqlite3
 
 # Returns the internal IP address of the current machine of which the server is to be hosted on 
 def getIP():
-	try:
-		ip = socket.gethostbyname(socket.getfqdn()) # return fully-qualified domain name
-	except:
-		ip = ''
-	# if (not ip) or (ip.startswith('127.')):
-	# 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create new socket 
+    try:
+        ip = socket.gethostbyname(socket.getfqdn())  # return fully-qualified domain name
+    except:
+        ip = ''
+    # if (not ip) or (ip.startswith('127.')):
+    #   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create new socket 
  #        s.connect(("8.8.8.8", 80)) # open socket to google's DNS server 
  #        ip = s.getsockname()[0] # take address from that connection 
    
-	return ip
+    return ip
+
 
 local_ip = getIP() # socket to listen  
 ext_ip = '122.62.141.222'
 #ip = "127.0.0.1"
-port = 10008 # TCP port to listen 
+port = 10008  # TCP port to listen 
 salt = "COMPSYS302-2017"
 db_file = 'app.db'
 curs = ''
 
-def connectDatabse(db_file): 
-	try:
-		conn = sqlite3.connect(db_file, check_same_thread=False)
-		print(sqlite3.version)
-	except Error as e:
-		print(e)
-	# finally: 
-	# 	conn.close()
-	return conn
+
+def connectDatabse(db_file):
+    try:
+        conn = sqlite3.connect(db_file, check_same_thread=False)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    # finally: 
+    #   conn.close()
+    return conn
+
 
 def createTable(conn, create_table_sql):
-	try: 
-		curs = conn.cursor() 
-		curs.execute(create_table_sql)
-	except Error as e:
-		print(e)
+    try:
+        curs = conn.cursor() 
+        curs.execute(create_table_sql)
+    except Error as e:
+        print(e)
 
-def createOnlineUserTable(db): 
-	users_table = """CREATE TABLE IF NOT EXISTS user_list ( id INTEGER PRIMARY KEY, username TEXT, location INTEGER, ip TEXT, port INTEGER, login_time TEXT);"""
-	if db is not None: 
-		createTable(db, users_table)
-		db.commit() 
-		return True
-	else:
-		print 'db connection not made!'
-		return False
+
+def createOnlineUserTable(db):
+    users_table = """CREATE TABLE IF NOT EXISTS user_list ( id INTEGER PRIMARY KEY, username TEXT, location INTEGER, ip TEXT, port INTEGER, login_time TEXT);"""
+    if db is not None:
+        createTable(db, users_table)
+        db.commit()
+        return True
+    else:
+        print 'db connection not made!'
+        return False
 
 # def formatUserList(response):
-# 	user_details = response.replace("0, Online user list returned", "")
+#   user_details = response.replace("0, Online user list returned", "")
 
-# 	user_details = user_details.split() 
-# 	for i in range (len(user_details)):
-# 		if (',' in user_details[i]):
-# 			split_details = user_details[i].split(',')
-# 			if (split_details[0] != cherrypy.session['username']):
-# 				print split_details[0]
-# 				insertUser(split_details, db, cursor)
-# 				# TODO: put in db 
+#   user_details = user_details.split() 
+#   for i in range (len(user_details)):
+#       if (',' in user_details[i]):
+#           split_details = user_details[i].split(',')
+#           if (split_details[0] != cherrypy.session['username']):
+#               print split_details[0]
+#               insertUser(split_details, db, cursor)
+#               # TODO: put in db 
 
 def insertUser(user_details, db, cursor): 
-	username = user_details[0]
-	cursor.execute('''SELECT * FROM user_list WHERE username=?''', (username,))
-	if (cursor.fetchone() == None):
-		location = user_details[1]
-		print location
-		ip = user_details[2]
-		port = user_details[3]
-		login_time = user_details[4]
-		cursor.execute('''INSERT INTO user_list (username, location, ip, port, login_time)
-		VALUES (?, ?, ?, ?, ?)''', (username, location, ip, port, login_time))
-		db.commit() 
+    username = user_details[0]
+    print username 
+    cursor.execute('''SELECT * FROM user_list WHERE username=?''', (username,))
+    if (cursor.fetchone() is None):
+        location = user_details[1]
+        ip = user_details[2]
+        port = user_details[3]
+        login_time = user_details[4]
+        cursor.execute('''INSERT INTO user_list (username, location, ip, port, login_time)
+        VALUES (?, ?, ?, ?, ?)''', (username, location, ip, port, login_time))
+        db.commit() 
 
 class MainApp(object):
-	msg = " "
-	global db 
-	db = connectDatabse(db_file)
-	global cursor 
-	cursor = db.cursor()
-	db_init = createOnlineUserTable(db)
+    msg = " "
+    global db 
+    db = connectDatabse(db_file)
+    global cursor 
+    cursor = db.cursor()
+    db_init = createOnlineUserTable(db)
 
 
-	@cherrypy.expose
-	def login(self):
-		page = open('main.html', 'r').read().format(message=self.msg)
-		#page = html.read()
-		logged_in = False
-		#page = self.checkLogin(page)
-		return page
+    @cherrypy.expose
+    def login(self):
+        page = open('main.html', 'r').read().format(message=self.msg)
+        #page = html.read()
+        logged_in = False
+        #page = self.checkLogin(page)
+        return page
 
-	@cherrypy.expose
-	def home(self):
-		page = open('loggedin.html', 'r').read().format(username=cherrypy.session['username'], user_list=self.getList())
-		#html.close()
-		#page = self.checkLogin(page)
-		return page
+    @cherrypy.expose
+    def home(self):
+        page = open('loggedin.html', 'r').read().format(username=cherrypy.session['username'], user_list=self.getList())
+        #html.close()
+        #page = self.checkLogin(page)
+        return page
 
-	@cherrypy.expose
-	def signin(self, username=None, password=None): 
-		hash_pw = hashlib.sha256(str(password+salt)).hexdigest()
-		error = self.authoriseLogin(username, hash_pw)
-		print error
-		if (int(error) == 0):
-			cherrypy.session['username'] = username
-			cherrypy.session['password'] = hash_pw 
-			self.t = threading.Thread(target=self.report, args=[cherrypy.session['username'], cherrypy.session['password'], False])
-			self.daemon = True
-			self.t.start()
-			raise cherrypy.HTTPRedirect('/home')
-		else:
-			print "login failed!2"
-			self.msg = "Incorrect credentials, please try again"
-			raise cherrypy.HTTPRedirect('/login')
+    @cherrypy.expose
+    def signin(self, username=None, password=None): 
+        hash_pw = hashlib.sha256(str(password+salt)).hexdigest()
+        error = self.authoriseLogin(username, hash_pw)
+        print error
+        if (int(error) == 0):
+            cherrypy.session['username'] = username
+            cherrypy.session['password'] = hash_pw 
+            self.t = threading.Thread(target=self.report, args=[cherrypy.session['username'], cherrypy.session['password'], False])
+            self.daemon = True
+            self.t.start()
+            raise cherrypy.HTTPRedirect('/home')
+        else:
+            print "login failed!2"
+            self.msg = "Incorrect credentials, please try again"
+            raise cherrypy.HTTPRedirect('/login')
 
-		
+       
+    @cherrypy.expose
+    def report(self, username, hash_pw, first_login):
+        response = 0
+        if (int(response) == 0):
+            #time.sleep(30)
+            try:
+                url = 'http://cs302.pythonanywhere.com/report?username=' + str(username)
+                url += '&password=' + str(hash_pw)  + '&location=' + '2' + '&ip=' + ext_ip # TODO: DON'T HARDCODE LOCATION
+                url += '&port=' + str(port) + '&enc=0'
+                print "logged in!"
+            except:
+                print "login failed!"
+                raise cherrypy.HTTPRedirect('/login')
+            # Getting the error code from the server
+            response_message = (urllib2.urlopen(url)).read()
+            response = str(response_message)[0]
+            # Display response message from the server
+            print "Server response: " + str(response_message)
+            return response
+         
 
-	@cherrypy.expose
-	def report(self, username, hash_pw, first_login):
-		response = 0
-		if (int(response) == 0):
-			#time.sleep(30)
-			try:
-				url = 'http://cs302.pythonanywhere.com/report?username=' + str(username)
-				url += '&password=' + str(hash_pw)  + '&location=' + '2' + '&ip=' + ext_ip # TODO: DON'T HARDCODE LOCATION
-				url += '&port=' + str(port) + '&enc=0'
-				print "logged in!"
-			except:
-				print "login failed!"
-				raise cherrypy.HTTPRedirect('/login')
-			# Getting the error code from the server
-			response_message = (urllib2.urlopen(url)).read()
-			response = str(response_message)[0]
-			# Display response message from the server
-			print "Server response: " + str(response_message)
-			return response
-		 
+    def authoriseLogin(self, username, hash_pw):
+        return self.report(username, hash_pw, True)
 
-	def authoriseLogin(self, username, hash_pw):
-		return self.report(username, hash_pw, True)
+    def checkLogin(self, page):
+        logged_in = True
+        try:
+            username = cherrypy.session['username']
+        except KeyError:
+            logged_in = False
 
-	def checkLogin(self, page):
-		logged_in = True
-		try:
-			username = cherrypy.session['username']
-		except KeyError:
-			logged_in = False
+        if (logged_in == True):
+            html = open('loggedin.html', 'r')
+            page = str(html.read())
+            html.close()
+            page = self.checkLogin(page)
 
-		if (logged_in == True):
-			html = open('loggedin.html', 'r')
-			page = str(html.read())
-			html.close()
-			page = self.checkLogin(page)
+        return page;
 
-		return page;
+    @cherrypy.expose
+    def signout(self):
+        try:
+            url = 'http://cs302.pythonanywhere.com/logoff?username=' + str(cherrypy.session['username']) + '&password=' + str(cherrypy.session['password']) + '&enc=0'
+        except: 
+            print 'logout failed'
+        response = (urllib2.urlopen(url)).read()
+        error = str(response)[0]
+        if (int(error) == 0):
+            self.msg = 'Logout successful!'
+            cherrypy.session.clear() # clear user session 
+            raise cherrypy.HTTPRedirect('/login')
 
-	@cherrypy.expose
-	def signout(self):
-		try:
-			url = 'http://cs302.pythonanywhere.com/logoff?username=' + str(cherrypy.session['username']) + '&password=' + str(cherrypy.session['password']) + '&enc=0'
-		except: 
-			print 'logout failed'
-		response = (urllib2.urlopen(url)).read()
-		error = str(response)[0]
-		if (int(error) == 0):
-			self.msg = 'Logout successful!'
-			cherrypy.session.clear() # clear user session 
-			raise cherrypy.HTTPRedirect('/login')
+    def getList(self): 
+        try: 
+            url = 'http://cs302.pythonanywhere.com/getList?username=' + str(cherrypy.session['username']) + '&password=' + str(cherrypy.session['password']) + '&enc=0'
+        except: 
+            print 'getList failed!'
+            raise cherrypy.HTTPRedirect('/')
 
-	def getList(self): 
-		try: 
-			url = 'http://cs302.pythonanywhere.com/getList?username=' + str(cherrypy.session['username']) + '&password=' + str(cherrypy.session['password']) + '&enc=0'
-		except: 
-			print 'getList failed!'
-			raise cherrypy.HTTPRedirect('/')
-
-		response = str((urllib2.urlopen(url)).read())
-		error = int(response[0])
-		if (error == 0):
-			user_list = response
-			page = ''
-			user_details = response.replace("0, Online user list returned", "")
-			user_details = user_details.split() 
-			for i in range (len(user_details)):
-				if (',' in user_details[i]):
-					split_details = user_details[i].split(',')
-					if (split_details[0] != cherrypy.session['username']):
-						print split_details[0]
-						insertUser(split_details, db, cursor)
-			return user_list
+        response = str((urllib2.urlopen(url)).read())
+        error = int(response[0])
+        if (error == 0):
+            user_list = response
+            usernames = []
+            page = ''
+            user_details = response.replace("0, Online user list returned", "")
+            user_details = user_details.split() 
+            for i in range (len(user_details)):
+                if (',' in user_details[i]):
+                    split_details = user_details[i].split(',')
+                    if (split_details[0] != cherrypy.session['username']):
+                        usernames.append(split_details[0])
+                        insertUser(split_details, db, cursor)
+            return ", ".join(usernames)
 
 
-	webbrowser.open_new('http://%s:%d/login' % (local_ip, port))
+    webbrowser.open_new('http://%s:%d/login' % (local_ip, port))
 
 def runMainApp():
-	conf = {
+    conf = {
          '/': {
              'tools.sessions.on': True,
              'tools.staticdir.root': os.path.abspath(os.getcwd())
@@ -216,20 +220,20 @@ def runMainApp():
              'tools.staticdir.on': True,
              'tools.staticdir.dir': './static'
          }
-	}
+    }
 
-	cherrypy.tree.mount(MainApp(), "/", conf)
+    cherrypy.tree.mount(MainApp(), "/", conf)
 
-	cherrypy.config.update({'server.socket_host': local_ip,
-						'server.socket_port': port,
-						#'engine.autoreload.on': True,
-						})
+    cherrypy.config.update({'server.socket_host': local_ip,
+                        'server.socket_port': port,
+                        #'engine.autoreload.on': True,
+                        })
 
 
-	cherrypy.engine.start() # start webserver
+    cherrypy.engine.start() # start webserver
 
-	cherrypy.engine.block() # stop doing anything else 
-	#cherrypy.engine.stop() # terminate; stop the channel of the bus 
-	cherrypy.server.unsubscribe() # disable built-in HTTP server 
+    cherrypy.engine.block() # stop doing anything else 
+    #cherrypy.engine.stop() # terminate; stop the channel of the bus 
+    cherrypy.server.unsubscribe() # disable built-in HTTP server 
 
 runMainApp()
