150b9dd6ceff6a1be357e841d1a10b735a4d7c50
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-06-06 02:18:33 +1200

    display confirmation of message send (need to test)

--- main.py
+++ main.py
@@ -269,12 +269,16 @@ class MainApp(object):
                 response = urllib2.urlopen(req).read()
                 print response
                 # print str(response)
-                # if (str(response[0]) == 0):
-                self.chat += '<div style="text-align:right">'
-                self.chat += 'You: ' + message + '<br></div>'
-                cursor.execute('''INSERT INTO messages (sender, recipient, message, stamp)
-                VALUES (?, ?, ?, ?)''', (cherrypy.session['username'], recipient, message, current_time))
-                db.commit()
+                if (str(response[0]) == 0):
+                    self.chat = 'Message sent!'
+                    cursor.execute('''INSERT INTO messages (sender, recipient, message, stamp)
+                    VALUES (?, ?, ?, ?)''', (cherrypy.session['username'], recipient, message, current_time))
+                    db.commit()
+                else:
+                    error = 'Message failed to send!'
+                    print error 
+                    self.chat = error
+
                 break
                 # else:
                 #     print 'could not send message!'
