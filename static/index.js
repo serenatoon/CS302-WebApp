8d0d859825fa40845e31e470ad85474c4423415e
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-06-11 01:55:12 +1200

    threading to make it update status?

--- static/index.js
+++ static/index.js
@@ -45,13 +45,13 @@ window.setInterval(function(){
            });
 }, 1000);
 
-// Poll for user status changes once every 10 seconds 
+//Poll for user status changes once every 2 minutes 
 window.setInterval(function(){
-  // var update_data = {username: personName} ;
+  //var update_data = {username: personName} ;
         $.post('/updateStatuses', function() {
                 //$('.chat.active-chat').html(people); 
            });
         $.post('/updatePeople', function(people) {
                 $('.people').html(people); // update the pane 
            });
-}, 1000);
+}, 120000);
